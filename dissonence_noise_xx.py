b6c71a556d691b34d312667e84fce8db5b0995f9
Martin Andreoni <74672942+martin-tii@users.noreply.github.com>
2023-05-10 00:54:37 +0400

    now we do a challenge response with a nonce from the server

--- dissonence_noise_xx.py
+++ dissonence_noise_xx.py
@@ -11,19 +11,61 @@ from dissononce.processing.impl.symmetricstate import SymmetricState
 from dissononce.processing.impl.cipherstate import CipherState
 from cryptography.hazmat.primitives import hashes
 from termcolor import colored
+import os
+from hashlib import blake2b
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
 
 dissononce.logger.setLevel(logging.ERROR)
 
+'''
+Protocol:
+handshake of noise xx (3 messages)
+nonce from server to client 
+answer from client to server --> hash of the fingerprint root certificate + nonce
+repeat from client to server
+'''
+
+
+def load_root(root_cert="root_cert.pem"):
+    # Load the root certificate from file
+    with open(root_cert, "rb") as f:
+        root_cert_data = f.read()
+    # Parse the root certificate
+    return x509.load_pem_x509_certificate(root_cert_data, default_backend())
 
 
 class noise_xx:
-    def send_cert(self, socket, handshake):
-        expected_cert_fingerprint_hex = self.load_root().fingerprint(hashes.SHA256())
-        message_buffer = handshake[1].encrypt_with_ad(b'', expected_cert_fingerprint_hex)
+
+    def __init__(self, debug=False):
+        self.debug = debug
+
+    def send_cert(self, socket, handshake, received_nonce, cert):
+        expected_cert_fingerprint_hex = cert.fingerprint(hashes.SHA256())
+        gfg = blake2b()
+        gfg.update(expected_cert_fingerprint_hex)
+        gfg.update(received_nonce)
+        message_buffer = handshake[1].encrypt_with_ad(b'', gfg.digest())
+        if self.debug:
+            print("Sent digest: ", gfg.digest())
         socket.sendall(bytes(message_buffer))
 
+    def send_nonce(self, sock, handshake):
+        nonce = os.urandom(100)
+        message_buffer = handshake[1].encrypt_with_ad(b'', nonce)
+        sock.sendall(message_buffer)
+        if self.debug:
+            print("Sent Nonce: ", nonce)
+        return nonce
 
-    def validate_cert(self, socket, handshake):
+    def receive_nonce(self, sock, handshake):
+        received = sock.recv(1024)
+        received_nonce = handshake[1].decrypt_with_ad(b'', received)
+        if self.debug:
+            print("Received Nonce: ", received_nonce)
+        return received_nonce
+
+    def validate_cert(self, socket, handshake, my_nonce):
         '''
         TODO: add x509 validation
         :param socket:
@@ -32,30 +74,22 @@ class noise_xx:
         '''
         # get root_certificate
         message_buffer = socket.recv(1024)
-        remote_certificate = handshake[1].decrypt_with_ad(b'', message_buffer)
-        expected_cert_fingerprint_hex = self.load_root().fingerprint(hashes.SHA256())
-        if remote_certificate != expected_cert_fingerprint_hex:
+        answer = handshake[1].decrypt_with_ad(b'', message_buffer)
+        expected_cert_fingerprint_hex = load_root().fingerprint(hashes.SHA256())
+        gfg = blake2b()
+        gfg.update(expected_cert_fingerprint_hex)
+        gfg.update(my_nonce)
+        if answer != gfg.digest():
             print(colored("Not Valid Client Certificate", 'red'))
             raise ValueError(
-                f"Server certificate fingerprint {remote_certificate} does not match expected fingerprint {expected_cert_fingerprint_hex}")
+                f"Response: {answer} does not match expected response: {gfg.digest()}")
         else:
-            print(colored('> Valid Certificate', 'green'))
+            print(colored('> Valid Response', 'green'))
+            if self.debug:
+                print(f"Challenge: {gfg.digest()},  Response: {answer}")
             return True
 
-
-    def load_root(self, ):
-        from cryptography import x509
-        from cryptography.hazmat.backends import default_backend
-
-        # Load the root certificate from file
-        with open("root_cert.pem", "rb") as f:
-            root_cert_data = f.read()
-
-        # Parse the root certificate
-        return x509.load_pem_x509_certificate(root_cert_data, default_backend())
-
-
-    def noise_client(self, IP):  # Alice
+    def noise_client(self, IP, certificate):  # Alice
         # set up logging and initialize Alice's keypair
         client_s = X25519DH().generate_keypair()
 
@@ -91,15 +125,20 @@ class noise_xx:
             client_cipherstates = client_handshakestate.write_message(b'', message_buffer)
             s.sendall(bytes(message_buffer))
 
-            self.send_cert(s, client_cipherstates)
+            received_nonce = self.receive_nonce(s, client_cipherstates)
 
-            self.validate_cert(s, client_cipherstates)
+            cert = load_root(certificate)
+            self.send_cert(s, client_cipherstates, received_nonce, cert)
 
+            mynonce = self.send_nonce(s, client_cipherstates)
+
+            if self.validate_cert(s, client_cipherstates, mynonce):
+                s.close()
+                return client_cipherstates
+            else:
+                s.close()
+                return "not valid"
             # close the socket
-            s.close()
-
-        return client_cipherstates
-
 
     def noise_server(self, queue):  # Bob
         server_s = X25519DH().generate_keypair()
@@ -142,11 +181,14 @@ class noise_xx:
         message_buffer = client_socket.recv(1024)
         server_cipherstates = server_handshakestate.read_message(bytes(message_buffer), bytearray())
 
-        if self.validate_cert(client_socket, server_cipherstates):
-            self.send_cert(client_socket, server_cipherstates)
+        mynonce = self.send_nonce(client_socket, server_cipherstates)
 
-        client_socket.close()
-        queue.put(server_cipherstates)
-        return server_cipherstates
-
-
+        if self.validate_cert(client_socket, server_cipherstates, mynonce):
+            received_nonce = self.receive_nonce(client_socket, server_cipherstates)
+            self.send_cert(client_socket, server_cipherstates, received_nonce, load_root())
+            queue.put(server_cipherstates)
+            client_socket.close()
+            return server_cipherstates
+        else:
+            client_socket.close()
+            return "not valid"
--- test-cases.py
+++ test-cases.py
@@ -5,27 +5,45 @@ from dissonence_noise_xx import noise_xx
 
 
 class Testing(unittest.TestCase):
-    noi = noise_xx()
-    q = queue.Queue()
-    # Start server
-    server_thread = threading.Thread(target=noi.noise_server, args=(q,))
-    server_thread.start()
-    # Get session from queue
-    client_session = noi.noise_client('localhost')
-    server_session = q.get()
+    def setUp(self) -> None:
+        self.noi = noise_xx(True)
 
-    # transport phase
-    # client to server
-    ciphertext = client_session[0].encrypt_with_ad(b'', b'Hello')
-    plaintext = server_session[0].decrypt_with_ad(b'', ciphertext)
-    print("Plaintext decrypted on server: ", plaintext)
-    assert plaintext == b'Hello'
+    def test_encrypt_decrypt(self):
+        q = queue.Queue()
+        # Start server
+        server_thread = threading.Thread(target=self.noi.noise_server, args=(q,))
+        server_thread.start()
+        # Get session from queue
+        client_session = self.noi.noise_client('localhost', "root_cert.pem")
+        server_session = q.get()
 
-    # bob to alice
-    ciphertext = server_session[1].encrypt_with_ad(b'', b'World')
-    plaintext = client_session[1].decrypt_with_ad(b'', ciphertext)
-    print("Plaintext decrypted on client: ", plaintext)
-    assert plaintext == b'World'
+        # transport phase
+        # client to server
+        ciphertext = client_session[0].encrypt_with_ad(b'', b'Hello')
+        plaintext = server_session[0].decrypt_with_ad(b'', ciphertext)
+        print("Plaintext decrypted on server: ", plaintext)
+        # assert plaintext == b'Hello'
+        self.assertEqual(plaintext, b'Hello')
+
+        # bob to alice
+        ciphertext = server_session[1].encrypt_with_ad(b'', b'World')
+        plaintext = client_session[1].decrypt_with_ad(b'', ciphertext)
+        print("Plaintext decrypted on client: ", plaintext)
+        # assert plaintext == b'World'
+        self.assertEqual(plaintext, b'World')
+
+    def test_fail_to_authenticate(self):
+        pass
+        #TODO
+        # q = queue.Queue()
+        # # Start server
+        # server_thread = threading.Thread(target=self.noi.noise_server, args=(q,))
+        # server_thread.start()
+        # # Get session from queue
+        # client_session = self.noi.noise_client('localhost', "fake_cert.pem")
+        # server_session = q.get()
+        # self.assertEqual(client_session, "not valid")
+        # self.assertEqual(server_session, "not valid")
 
 
 if __name__ == '__main__':
